c05d02a4bce5771e9a95ba1ec412563b828de67d
Omar Oseguera <OmarO92@users.noreply.github.com>
2020-04-01 16:11:21 -0700

    sections-5-6

---
+++ code-section-6/code/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'my_secret'
+api = Api(app)
+
+# this code will run before first request runs
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':  # code will only execute if app is ran
+    db.init_app(app)
+    app.run(port=5000, debug=True)
